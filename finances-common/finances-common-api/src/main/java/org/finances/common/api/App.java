54da1f18202219c1388203fd27bf2ebb3722b323
wangyuxin <youngmanpoorman@163.com>
2018-06-07 11:45:26 +0800

    g

---
+++ finances-common/finances-common-api/src/main/java/org/finances/common/api/App.java
@@ -0,0 +1,13 @@
+package org.finances.common.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
